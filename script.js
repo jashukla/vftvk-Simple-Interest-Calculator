8634f6c6c9a8ff0663b2d42c2142f82703cf4f21
Jatashankar Shukla <jashukla@in.ibm.com>
2022-04-04 17:57:23 +0000

    vlidation on form submit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if(principal<= 0){
+        alert("Enter a positive number");
+    }
+    document.getElementById("principal").focus();
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
