c968ad5f58df300be42ad65277bebcf331211655
Jatashankar Shukla <jashukla@in.ibm.com>
2022-04-04 18:00:38 +0000

    vlidation on form submit

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ function compute()
     var principal = document.getElementById("principal").value;
     if(principal<= 0){
         alert("Enter a positive number");
-    }
-    document.getElementById("principal").focus();
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    }else{
+        document.getElementById("principal").focus();
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
     
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        
+    }
+   
     
 }
 function validatePrincipal(){ 
